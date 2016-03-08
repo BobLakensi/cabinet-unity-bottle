3003aaf79826a9dcce54af683c5bbcdab2afe713
Monte <BobLakensi@users.noreply.github.com>
2016-03-07 19:17:00 -0800

    Added Info Buttons

--- girls-who-code-website/app.js
+++ girls-who-code-website/app.js
@@ -1,3 +1,11 @@
-function testButton (button){
+function sideBarButtonSA (button){
+    console.log("gdahsuashf");
+}
+
+function sideBarButtonI (button){
+    console.log("gdahsuashf");
+}
+
+function sideBarButtonNA (button){
     console.log("gdahsuashf");
 }
