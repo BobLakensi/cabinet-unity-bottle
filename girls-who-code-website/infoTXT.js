e78dfb24114601fedaa223b2c13cd8a3339aa42f
Monte <BobLakensi@users.noreply.github.com>
2016-03-31 19:22:22 -0700

    Made each category in NA its own object with more values

--- girls-who-code-website/infoTXT.js
+++ girls-who-code-website/infoTXT.js
@@ -1,11 +1,37 @@
 var info = [
     {"period": "North America", 
-     "environment": "The four major regions were  the American Southwest,the northern coast and Alaska, the Great Plains, and the Northeast. North America has been divided into cultural regions based on language and social and artistic patterns.", 
-    "military/political": "There were many different tribes and nations living throughout North America. aAter the arrival of the europeans and the Native Americans being forced out of their land they were soon forced to assimilate into the white culture.",
-     "intellectual/artistic": "Many parts of the indigenous cultures of the United States and Canada have to get back to as far as 12,000 years ago, although many of the surviving objects date from the past two thousand years. Art and architecture were made from local products. Early Native American artists excelled in working stone into a variety of utilitarian and ceremonial objects. Effigy mounds were also made in respect of nature and agricultural fertility.",
-     "religion": "Nature was very important in the information society and religion and it was respected by the Native Americans. Many rules were made about nature. Shamanic rituals and dream guidance were a big part of Native American religion. Message Eman mermaid following the beliefs on respect of nature and also used in religious ceremonies.",
-     "economic/social": "After the Native Americans were from their land, they made everything for the white market and to survive. Lifestyles of the people there is throughout the entire country. What is the cultures existed separately not together and they were stimulated to the white culture after the arrival of the Europeans. The ideas of sharing, no personal property and living together were prominent in the Native American culture. Status was by leadership and military achievements rather than money and land."
+     "mapIMG": "http://www.timemaps.com/store/timemaps/2012/4/northamerica1500bc.jpg",
+     "environment":  {
+         "text": "The four major regions were  the American Southwest,the northern coast and Alaska, the Great Plains, and the      Northeast. North America has been divided into cultural regions based on language and social and artistic patterns.", 
+         "img": "",
+         "caption": ""
+     },
+     
+    "military/political": {
+        "text" : "There were many different tribes and nations living throughout North America. aAter the arrival of the europeans and the Native Americans being forced out of their land they were soon forced to assimilate into the white culture.",
+        "img": "",
+        "caption": ""
     },
+     
+     "intellectual/artistic": {
+         "text" : "Many parts of the indigenous cultures of the United States and Canada have to get back to as far as 12,000 years ago, although many of the surviving objects date from the past two thousand years. Art and architecture were made from local products. Early Native American artists excelled in working stone into a variety of utilitarian and ceremonial objects. Effigy mounds were also made in respect of nature and agricultural fertility.",
+         "img": "",
+         "caption": ""
+     },
+     
+     "religion": {
+         "text": "Nature was very important in the information society and religion and it was respected by the Native Americans. Many rules were made about nature. Shamanic rituals and dream guidance were a big part of Native American religion. Message Eman mermaid following the beliefs on respect of nature and also used in religious ceremonies.",
+         "img": "",
+         "caption": ""
+     },
+    
+     
+     "economic/social": { 
+        "text" : "After the Native Americans were from their land, they made everything for the white market and to survive. Lifestyles of the people there is throughout the entire country. What is the cultures existed separately not together and they were stimulated to the white culture after the arrival of the Europeans. The ideas of sharing, no personal property and living together were prominent in the Native American culture. Status was by leadership and military achievements rather than money and land.",
+         "img": "",
+         "caption": ""
+    }
+    },  //North America
     
     {"period": "South America",
      "environment": "The central Andean region of South America lies in between Ecuador and northern Chile, with its western border to the Pacific ocean. There are three well-defined geographic zones: a narrow western coastal plain is a hot desert crossed by rivers, creating fertile valleys; high peaks of the great Cordillera of the Andes hem in plateaus of a temperate climate, and the inland border, the eastern slopes of the Andes, which is a hot and humid jungle. Civilizations flourished in the highlands and the coast. ",
@@ -13,7 +39,7 @@ var info = [
      "intellectual/artistic": "Despite the diversity of culture and language, the indigenous people shared religion, technology, culture, traditions and ideological beliefs. The Chavin was the mother culture of this region. Iconography spread widely through gold, textiles and ceramic. Metalwork was developed and they built monumental architecture. Mummification was used 500 years before the Egyptians. Organized labor led to the ability to construct monumental architecture. Composite pictures and the concept of duality were important. In this society Andes mountains were also very important as they are the source of life giving rivers which these communities dependent for their survival. The head no written language but an elaborate road system",
      "religion": "During the Spanish conquest, many religious items were destroyed. The jaguar isn't important spiritual than religious animal as it has night vision using shamanism is connected to spiritual beliefs and it's a vicious animal. There was also an interest in the afterlife.",
      "economic/social": "During the Spanish conquest, gold artifacts were melted down and sent to Spain. Fertile land allowed for agriculture and a food surplus. This food surplus led to specialization and the development of culture. The communities were planned and boasted organized labor systems. ",
-    },
+    },  //South America
     
     {"period": "Inkas",
      "environment": "The Inkans established  themselves in the Cusco Valley around 1000. In the 15th century, they rapidly expanded their power until they reached from modern Quito, Ecuador to central Chile, an area of over 3,000 miles. By the time of the Spanish conquistadors in 1519 the Inkans were the largest empire in the world. The empire was divided into four quarters with section converging to Cusco. ",
@@ -21,7 +47,7 @@ var info = [
      "intellectual/artistic": "The Chavin were the mother culture of the Inkas. The Inkans were experts in mining and metalwork. The master techniques in agriculture to adapt to their harsh environment. They developed terracing and irrigation for agriculture. They built roads and bridges to maintain their large empire. This 14,000 miles of infrastructure was used in the transportation goods throughout their vast Empire. A system of communication through relay runners were used carry messages throughout the Empire, but had no written language. They had no writing system but used the khipu for recording history, celestial and astronomical information, census and tribute totals, and inventories. They made many advances in mathematics and the sciences.  ",
      "religion": "Inkan religion was based off of the sun and ideas about the axis mundi. They had a  polytheistic religion with deities base off nature and observation of astronomy. Shrines were built to honor and worship these dieties. ",
      "economic/social": "The Inkans' expertise in mining and metalwork enabled them to accumulate enormous wealth of gold and silver. When does Spanish conquistadores invaded in 1519 they stripped the Incans of many of the riches in gold silver and other minerals. Clothing often was used to show status. The society was highly hierarchical and there was little social mobility. ",
-    },
+    },  //Inkas
     
     {"period": "Rococo",
      "environment": "France was the main home of Rococo art and architecture, although they were influenced by many things outside. Paris was the center of the Rococo style and culture. The American Revolution started in America and the Industrial Revolution began in England.",
@@ -29,7 +55,7 @@ var info = [
      "intellectual/artistic": "The term Rococo derives from <i> rocaille</i>, meaning pebble, referring to the use of small stones and shells used to decorate interiors. The patrons of Rococo art and architecture were often wealthy, educated female aristocrats, which caused art to become more “feminine”. Sensuous themes were popular and they were extravagant to show wealth and status. Light colors, including pink, red, and pastel colors, were used following the ideas of sensuality and female patrons. Curvilinear lines, visible brushstrokes, and an outdoor setting were popular in Rococo art and architecture. The art was artificial and scenes of love and pleasure increased. ",
      "religion": "King Louis XIV destroyed Protestantism in France, so Catholicism was more popular. Religion wasn’t a big focus of Rococo art or architecture. ",
      "economic/social": "The warfare and fighting King Louix XIV got France into bankrupted the country. A rise in capitalism fueled by the Industrial Revolution cause a gain in economic power which led to a gain in political power. The death of King Louis XIV in 1715 led to many changes in the French high society. People socialized in salons, which led to the spread of information and arts. The middle class was growing and demanded political power. ",
-    },  
+    },  //Rococo
     
     {"period": "Enlightenment",
      "environment": "The Enlightenment started in Europe, with Isaac Newton and John Locke in Great Britain, but their ideas soon spread around the world. Ideas from the Enlightenment spread to the Americas, causing Revolutions there. There were also Enlightenment thinkers in the Americas, including Thomas Jefferson and Benjamin Franklin. The Industrialization caused colonization. The Europeans colonized for land and resources. ",
@@ -37,7 +63,7 @@ var info = [
      "intellectual/artistic": "Rationality and critical thinking based on logic and science instead of faith became the core Enlightenment ideas. The Enlightenment stemmed from many ideas from the Scientific Revolution. Isaac Newton developed the scientific method and encouraged others to avoid metaphysics and the supernatural. This emphasis on tangible data and concrete experience became a major idea of the Enlightenment. Diderot organized the first encyclopedia, a book containing all human knowledge. Galileo developed the heliocentric theories and made advances in astronomy. Science became included in art, as seen in the realism and detail, natural landscape, focus on accurate anatomy, and the light of God being replaced by the light of science. Iron and the machine were improved upon, causing an increase in architecture and mass production. ",
      "religion": "During this time, religion declines for science and philosophy. Ideas about people being able to change their destiny, they’re in control of it, and not God, was a big Enlightenment idea.  ",
      "economic/social": "The Enlightenment led to the Industrial Revolution. Countries that industrialized gained wealth and capitalism became more popular. By the end of the 18th century, there was major economic change and new wealth from industrial manufacturing. People tried to use Enlightenment ideas to fix society, abolish slavery, and gain women’s rights. The Industrial Revolution caused urbanization, colonization, ideas of Social Darwinism and Manifest Destiny. ",
-    },   
+    },  //Enlightenment
     
     {"period": "Neoclassical",
      "environment": "Neoclassicism started in Europe but soon spread out to the Americas, as classical ideals served as models for many American aspects of art, architecture, and government. ",
@@ -45,6 +71,6 @@ var info = [
      "intellectual/artistic": "A readmirization of the classical cultures caused them to be thought as the pinnacle of society. The Enlightenment’s emphasis on rationality probably added to the praising of the classical cultures as they embodied Enlightenment ideals. Revolutions, Reason, Rationality, and Roman (The 4 R’s) ideas grew in importance. The excavation of Pompeii and Herculaneum further whetted the appetite for classicism. The ancient world became a focus of study. Johann Winckelmann, the first modern art historian, published books on art and said it to be the most perfect form of art made by man. Edward Gibbon published his book on ancient Rome in 1776. They both helped spread Neoclassicism. Neoclassic art Incorporated subjects and styles of ancient art painting sculpture and architecture was the most prominent manifestations of Neoclassicism. Art was rational, attention was paid to detail, there was often a grand theme with morals involved. Many classical elements were used and there was a bigger focus on the line and perspective. ",
      "religion": "Religion lost much of its importance as a result of the Enlightenment’s ideas",
      "economic/social": "The classical cultures emphasized civic virtue, sacrifice, morality, patriotism, and liberty and became models during this period. The Grand Tour was fueling this interest in the classical cultures.",
-    }
+    }   //Neoclassical
     
 ]
