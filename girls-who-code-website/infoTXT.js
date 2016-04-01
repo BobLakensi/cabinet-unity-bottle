6ce76240e09f2d7393855ff67af94516afcb5c40
Monte <BobLakensi@users.noreply.github.com>
2016-03-31 17:53:54 -0700

    Added native americas info to infoTXT.js

--- girls-who-code-website/infoTXT.js
+++ girls-who-code-website/infoTXT.js
@@ -5,6 +5,46 @@ var info = [
      "intellectual/artistic": "Many parts of the indigenous cultures of the United States and Canada have to get back to as far as 12,000 years ago, although many of the surviving objects date from the past two thousand years. Art and architecture were made from local products. Early Native American artists excelled in working stone into a variety of utilitarian and ceremonial objects. Effigy mounds were also made in respect of nature and agricultural fertility.",
      "religion": "Nature was very important in the information society and religion and it was respected by the Native Americans. Many rules were made about nature. Shamanic rituals and dream guidance were a big part of Native American religion. Message Eman mermaid following the beliefs on respect of nature and also used in religious ceremonies.",
      "economic/social": "After the Native Americans were from their land, they made everything for the white market and to survive. Lifestyles of the people there is throughout the entire country. What is the cultures existed separately not together and they were stimulated to the white culture after the arrival of the Europeans. The ideas of sharing, no personal property and living together were prominent in the Native American culture. Status was by leadership and military achievements rather than money and land."
+    },
+    
+    {"period": "South America",
+     "environment": "The central Andean region of South America lies in between Ecuador and northern Chile, with its western border to the Pacific ocean. There are three well-defined geographic zones: a narrow western coastal plain is a hot desert crossed by rivers, creating fertile valleys; high peaks of the great Cordillera of the Andes hem in plateaus of a temperate climate, and the inland border, the eastern slopes of the Andes, which is a hot and humid jungle. Civilizations flourished in the highlands and the coast. ",
+     "military/political": "In 1519, Spanish conquistadors Francisco Pizarro and Hernando Cortez arrive in the Americas where the Spanish crushed the natives in violent confrontations. Before the arrival of the Spanish, there was conflict between the various native groups.",
+     "intellectual/artistic": "Despite the diversity of culture and language, the indigenous people shared religion, technology, culture, traditions and ideological beliefs. The Chavin was the mother culture of this region. Iconography spread widely through gold, textiles and ceramic. Metalwork was developed and they built monumental architecture. Mummification was used 500 years before the Egyptians. Organized labor led to the ability to construct monumental architecture. Composite pictures and the concept of duality were important. In this society Andes mountains were also very important as they are the source of life giving rivers which these communities dependent for their survival. The head no written language but an elaborate road system",
+     "religion": "During the Spanish conquest, many religious items were destroyed. The jaguar isn't important spiritual than religious animal as it has night vision using shamanism is connected to spiritual beliefs and it's a vicious animal. There was also an interest in the afterlife.",
+     "economic/social": "During the Spanish conquest, gold artifacts were melted down and sent to Spain. Fertile land allowed for agriculture and a food surplus. This food surplus led to specialization and the development of culture. The communities were planned and boasted organized labor systems. ",
+    },
+    
+    {"period": "Inkas",
+     "environment": "The Inkans established  themselves in the Cusco Valley around 1000. In the 15th century, they rapidly expanded their power until they reached from modern Quito, Ecuador to central Chile, an area of over 3,000 miles. By the time of the Spanish conquistadors in 1519 the Inkans were the largest empire in the world. The empire was divided into four quarters with section converging to Cusco. ",
+     "military/political": "The Inkans' expertise in mining and metalwork enabled them to accumulate enormous wealth of metals for weaponry. In 1542, Francisco Pizarro killed the Inkan successor after taking all of the empire's riches.  Over 12 million people into the Inkan empire. The size of the Inkan empire required and controllers to be skillfully organized and have administrative control. ",
+     "intellectual/artistic": "The Chavin were the mother culture of the Inkas. The Inkans were experts in mining and metalwork. The master techniques in agriculture to adapt to their harsh environment. They developed terracing and irrigation for agriculture. They built roads and bridges to maintain their large empire. This 14,000 miles of infrastructure was used in the transportation goods throughout their vast Empire. A system of communication through relay runners were used carry messages throughout the Empire, but had no written language. They had no writing system but used the khipu for recording history, celestial and astronomical information, census and tribute totals, and inventories. They made many advances in mathematics and the sciences.  ",
+     "religion": "Inkan religion was based off of the sun and ideas about the axis mundi. They had a  polytheistic religion with deities base off nature and observation of astronomy. Shrines were built to honor and worship these dieties. ",
+     "economic/social": "The Inkans' expertise in mining and metalwork enabled them to accumulate enormous wealth of gold and silver. When does Spanish conquistadores invaded in 1519 they stripped the Incans of many of the riches in gold silver and other minerals. Clothing often was used to show status. The society was highly hierarchical and there was little social mobility. ",
+    },
+    
+    {"period": "Rococo",
+     "environment": "",
+     "military/political": "",
+     "intellectual/artistic": "",
+     "religion": "",
+     "economic/social": "",
+    },  
+    
+    {"period": "Enlightenment",
+     "environment": "",
+     "military/political": "",
+     "intellectual/artistic": "",
+     "religion": "",
+     "economic/social": "",
+    },   
+    
+    {"period": "Neoclassical",
+     "environment": "",
+     "military/political": "",
+     "intellectual/artistic": "",
+     "religion": "",
+     "economic/social": "",
     }
     
 ]
